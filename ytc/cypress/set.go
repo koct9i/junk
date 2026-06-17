fe3597fa9007712d8c7e4d3109422964670add7d
Konstantin Khlebnikov <koct9i@gmail.com>
2026-06-17 23:27:37 +0200

    Use cli positional arguments for Cypress commands

---
+++ ytc/cypress/set.go
@@ -0,0 +1,31 @@
+package cypress
+
+import (
+	"context"
+
+	"github.com/urfave/cli/v3"
+	"go.ytsaurus.tech/yt/go/ypath"
+)
+
+func setCommand() *cli.Command {
+	return &cli.Command{
+		Name:  "set",
+		Usage: "set Cypress node value from argument or stdin",
+		Arguments: []cli.Argument{
+			&cli.StringArgs{Name: "path", Min: 1, Max: 1},
+			&cli.StringArgs{Name: "value", Min: 0, Max: 1},
+		},
+		Flags: flags(),
+		Action: func(ctx context.Context, c *cli.Command) error {
+			yc, err := client()
+			if err != nil {
+				return err
+			}
+			value, err := readValue(c.StringArgs("value"))
+			if err != nil {
+				return err
+			}
+			return yc.SetNode(ctx, ypath.Path(c.StringArgs("path")[0]), value, nil)
+		},
+	}
+}
